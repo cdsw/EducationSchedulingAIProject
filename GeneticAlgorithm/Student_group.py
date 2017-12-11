4b62b1e574271e84e89cca5a4ae20373d9758852
tigerSI <sirapop.tiger@gmail.com>
2017-12-11 21:18:00 +0700

    Create Genetic algorithm

--- GeneticAlgorithm/Student_group.py
+++ GeneticAlgorithm/Student_group.py
@@ -2,14 +2,10 @@ class StudentGroup:
 
     def __init__(self, student_group):
         self.student_group = student_group
-        self.student_group_timeslot=[0,0,0, 0,0,0, 0,0,0, 0,0,0, 0,0,0, 0,0,0, 0,0,0]
+        self.student_group_timeslot = [[],[],[], [],[],[], [],[],[], [],[],[], [],[],[], [],[],[], [],[],[]]
 
     def place_course(self, position, course):
-        if (self.student_group_timeslot[position] == 0):
-            self.student_group_timeslot[position] = [course]
-
-        else:
-            self.student_group_timeslot[position].append(course)
+        self.student_group_timeslot[position].append(course)
 
     def print_timeslot(self):
         count = 0
